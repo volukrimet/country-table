cd395337e9379dfbbe2cf3684ca9abcad42b21fd
Marsel <temirkulov324@gmail.com>
2022-01-31 22:06:15 +0600

    Second commit

--- main.js
+++ main.js
@@ -1,57 +1,11 @@
 const btnGet = $('button');
 const myTable = $('#table');
 const headers = ['Code', 'Flag', 'Name', 'Capital', 'Population'];
-let countries = []
 
 $.ajax({
     method: "GET",
     url: 'https://restcountries.com/v3.1/all'
 }).done(getCountries);
-
-setTimeout(() => {
-    btnGet.removeAttr('disabled');
-}, 3000);
-
-btnGet.on('click', () => {
-    const table = $('<table>');
-    const headerRow = $('<tr>');
-
-    headers.forEach(headerText => {
-        let header = $('<th>');
-        header.text(headerText);
-        headerRow.append(header);
-    });
-
-    table.append(headerRow);
-
-    countries.forEach(country => {
-        let row = $('<tr>');
-        let img = $('<img>');
-        let imgCell = $('<td>');
-
-        appendElement(row, country.cca2);
-
-        img.attr('src', country.flags.png);
-        img.attr('width', 200);
-        imgCell.append(img);
-        imgCell.css({ padding: 0 });
-        row.append(imgCell);
-
-        appendElement(row, country.name.common);
-
-        if (!country.capital)
-            appendElement(row, 'no data');
-        else appendElement(row, country.capital);
-
-        appendElement(row, country.population);
-
-        table.append(row);
-
-
-    })
-    myTable.append(table);
-    btnGet.css('display', 'none');
-});
 
 function appendElement(element, string) {
     element.append(createTd(string));
@@ -64,6 +18,48 @@ function createTd(string) {
 }
 
 function getCountries(response) {
-    response.map(country => countries.push(country));
-    countries.sort((a, b) => a.name.common > b.name.common ? 1 : -1);
+    btnGet.removeAttr('disabled')
+
+    let countries = response.sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
+
+    btnGet.on('click', () => {
+        const table = $('<table>');
+        const headerRow = $('<tr>');
+
+        headers.forEach(headerText => {
+            let header = $('<th>');
+            header.text(headerText);
+            headerRow.append(header);
+        });
+
+        table.append(headerRow);
+
+        countries.forEach(country => {
+            let row = $('<tr>');
+            let img = $('<img>');
+            let imgCell = $('<td>');
+
+            appendElement(row, country.cca2);
+
+            img.attr('src', country.flags.png);
+            img.attr('width', 200);
+            imgCell.append(img);
+            imgCell.css({ padding: 0 });
+            row.append(imgCell);
+
+            appendElement(row, country.name.common);
+
+            if (!country.capital)
+                appendElement(row, 'no data');
+            else appendElement(row, country.capital);
+
+            appendElement(row, country.population);
+
+            table.append(row);
+
+
+        })
+        myTable.append(table);
+        btnGet.css('display', 'none');
+    });
 }
